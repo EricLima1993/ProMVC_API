ab04cf9b34c2aefc4a2fdba3e2debac3520a91ca
EricLima1993 <Merick_lima@hotmail.com>
2021-08-27 14:47:08 -0300

    Ajustes na CategoriaController

--- Controllers/CategoriaController.cs
+++ Controllers/CategoriaController.cs
@@ -51,7 +51,7 @@ namespace ProMVC_API.Controllers
                 return BadRequest();
             }
 
-            _context.Entry(categoriaModel).State = EntityState.Modified;
+            _context.SetModified(categoriaModel);
 
             try
             {
